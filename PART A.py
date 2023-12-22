f85b7b29b3848f329473931f4ea02a404bd18be8
KRUSHITHA22 <144662964+KRUSHITHA22@users.noreply.github.com>
2023-12-22 18:52:00 +0530

    Add files via upload

---
+++ PART A.py
@@ -0,0 +1,18 @@
+die_faces = [1, 2, 3, 4, 5, 6]
+
+# Part-A: Total Combinations
+total_combinations = len(die_faces) ** 2
+print("Total Combinations:", total_combinations)
+
+# Part-A: Distribution of Possible Combinations
+combinations_distribution = [(i, j) for i in die_faces for j in die_faces]
+print("\nCombinations Distribution:")
+for i in range(0, total_combinations, len(die_faces)):
+    print(combinations_distribution[i:i + len(die_faces)])
+
+# Part-A: Probability of Sums
+print("\nProbability of Sums:")
+for i in range(2, 13):
+    count = sum(1 for face in combinations_distribution if sum(face) == i)
+    probability = count / total_combinations
+    print(f"P(Sum = {i}): {count}/{total_combinations} = {probability:.2f}")
---
+++ PART B.py
@@ -0,0 +1,10 @@
+def undoom_dice(die_a, die_b):
+    scaling_factor = sum(die_a) / sum(die_b)
+    new_die_a = [min(4, spots) for spots in die_a]
+    new_die_b = [min(6, round(spots * scaling_factor)) for spots in die_b]
+    return new_die_a, new_die_b
+die_a = [1, 2, 3, 4, 5, 6]
+die_b = die_a
+new_die_a, new_die_b = undoom_dice(die_a, die_b)
+print("\nNew Die A:", new_die_a)
+print("New Die B:", new_die_b)
